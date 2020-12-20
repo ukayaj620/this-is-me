4ff538991670335e90415ced5ff5d6073e6e6fd4
ukayaj620 <51906567+ukayaj620@users.noreply.github.com>
2020-12-20 19:04:38 +0700

    Revert "chore: setup tailwindcss, postcss, autoprefixer, delete unused file"
    This reverts commit 8a9e003424db69a634063553899954902fadd7ca.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import './styles/output.css';
+import logo from './logo.svg';
+import './App.css';
 
-const App = () => {
+function App() {
   return (
-    <div className="w-screen h-screen flex flex-col justify-center items-center">
-      <h1 className="text-6xl">Hello I'm Jayaku Briliantio</h1>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
