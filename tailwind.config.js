2cad784c530167809d41f416cf3e5c752992caee
ukayaj620 <51906567+ukayaj620@users.noreply.github.com>
2020-12-22 01:14:48 +0700

    feat: extend tailwind font and color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        primary: ['Quicksand', 'sans-serif'],
+      },
+      colors: {
+        primary: '#073BC8',
+      }
+    },
   },
   variants: {
     extend: {},
