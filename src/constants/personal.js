039403871f7be540806dc091701ee614dc881028
ukayaj620 <51906567+ukayaj620@users.noreply.github.com>
2020-12-22 18:32:03 +0700

    fix: export default error

--- src/constants/personal.js
+++ src/constants/personal.js
@@ -1,24 +1,28 @@
-export default {
-  profile: {
-    header: "Hi, I'm Jayaku Briliantio",
-    content: "I\'m a freelance full stack developer and deep learning enthusiast. I\'m also a Computer Science Student majoring in Mobile Application Technology at Bunda Mulia University.",
-  },
-  socialInfo: [
-    {
-      icon: 'github',
-      url: 'https://github.com/ukayaj620',
+const personal = () => {
+  return ({
+    profile: {
+      header: "Hi, I'm Jayaku Briliantio",
+      content: "I'm a freelance full stack developer and deep learning enthusiast. I'm also a Computer Science Student majoring in Mobile Application Technology at Bunda Mulia University.",
     },
-    {
-      icon: 'linkedin',
-      url: 'https://www.linkedin.com/in/neartojayakubriliantio',
-    },
-    {
-      icon: 'telegram',
-      url: 'https://t.me/jayakubriliantio',
-    },
-    {
-      icon: 'whatsapp',
-      url: 'https://wa.me/6281375302111'
-    },
-  ],
-}
+    socialInfo: [
+      {
+        icon: 'github',
+        url: 'https://github.com/ukayaj620',
+      },
+      {
+        icon: 'linkedin',
+        url: 'https://www.linkedin.com/in/neartojayakubriliantio',
+      },
+      {
+        icon: 'telegram',
+        url: 'https://t.me/jayakubriliantio',
+      },
+      {
+        icon: 'whatsapp',
+        url: 'https://wa.me/6281375302111'
+      },
+    ],
+  });
+};
+
+export default personal;
