f90045bf5e7640e09d3309590b78fe533a11e23b
ukayaj620 <51906567+ukayaj620@users.noreply.github.com>
2020-12-23 14:47:12 +0700

    feat: add target and rel attr to anchor

--- src/components/Anchor.jsx
+++ src/components/Anchor.jsx
@@ -1,18 +1,22 @@
 import { icons } from '../helpers/loader';
 
-const Anchor = ({ url, text }) => {
+const Anchor = ({ url, text, withLogo, bold }) => {
   return (
     <div className="flex flex-row items-center">
-      <a href={url}>
-        <h5 className="hover:text-purple-800 text-center text-primary mr-1">
+      <a
+        href={url}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <h5 className={`hover:text-purple-800 text-center text-primary mr-1 ${bold && 'font-semibold'}`}>
           {text}
         </h5>
       </a>
-      <img
+      {withLogo && <img
         src={icons('link')}
         alt={`link to ${url}`}
         className="w-4 h-4 lg:w-5 lg:h-5"
-      />
+      />}
     </div>
   );
 };
