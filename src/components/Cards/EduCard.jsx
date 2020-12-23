28557548d0fc42f40a5f86b516e587c356c927fd
ukayaj620 <51906567+ukayaj620@users.noreply.github.com>
2020-12-23 13:40:36 +0700

    feat: update tailwind style

--- src/components/Cards/EduCard.jsx
+++ src/components/Cards/EduCard.jsx
@@ -2,7 +2,7 @@ import { images } from "../../helpers/loader";
 
 const EduCard = ({ image, title, major, year, description }) => {
   return (
-    <div className="w-full my-3 px-6 py-2 flex flex-col lg:flex-row items-center">
+    <div className="w-full my-3 lg:px-6 py-2 flex flex-col lg:flex-row items-center">
       <img
         className="w-32 h-32 my-2 lg:mr-12"
         src={images(image)}
@@ -12,7 +12,7 @@ const EduCard = ({ image, title, major, year, description }) => {
         <h3>{title}</h3>
         <div className="flex flex-col font-semibold my-1 items-center lg:items-start">
           <h5 className="text-center text-gray-900 lg:mr-2">{major}</h5>
-          <h5 className="text-gray-500">[{year}]</h5>
+          <h5 className="text-gray-500 font-semibold">[ {year} ]</h5>
         </div>
         <h5 className="text-center lg:text-justify text-gray-900">{description}</h5>
       </div>
